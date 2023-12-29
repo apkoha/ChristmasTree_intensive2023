6744aa834437678562177def4a6ef7bc14ccc6cf
apkoha <79258179+apkoha@users.noreply.github.com>
2023-12-29 19:31:23 +0300

    завершение вёрстки + анимация снега

---
+++ snow/show.js
@@ -0,0 +1,62 @@
+const getRandowIntInclusive = (n, m) => {
+  const min = Math.min(n, m);
+  const max = Math.max(n, m);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const createSnow = (min, max, saturation) => {
+  const style = document.createElement("style");
+  document.head.append(style);
+
+  style.textContent = `
+    body {
+      position: relative;
+    }
+
+    .snow {
+      position: fixed;
+      width: 20px;
+      height: 20px;
+      top: -20px;
+      background-repeat: no-repeat;
+      background-position: center;
+      pointer-events: none;
+      animation-name: fall;
+      animation-timing-function: linear;
+    }
+
+    @keyframes fall {
+      100% {
+        transform: translateY(100vh)
+      }
+    }
+  `;
+
+  const count = 4;
+
+  const createSnowItem = () => {
+    const snowItem = document.createElement("div");
+    snowItem.classList.add("snow");
+
+    const time = getRandowIntInclusive(min, max);
+
+    snowItem.style.cssText = `
+    left: ${getRandowIntInclusive(0, 100)}%;
+    background-image: url("/snow/snowflake${getRandowIntInclusive(
+      1,
+      count
+    )}.svg");
+    animation-duration: ${time}s;
+    `;
+
+    document.body.append(snowItem);
+
+    setTimeout(() => {
+      snowItem.remove();
+    }, time * 1000);
+  };
+
+  setInterval(createSnowItem, saturation);
+};
+
+createSnow(5, 20, 500);
